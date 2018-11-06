93a7b3170d38e4d6018cbf0465fc360ca0d3540c
chaseeddie <38984786+chaseeddie@users.noreply.github.com>
2018-11-06 16:27:28 -0600

    Add files via upload

---
+++ login_page.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+
+# this is our homepage
+
+@app.route('/', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        attempted_user = request.form['username']
+        attempted_pass = request.form['password']
+
+        if attempted_user == 'admin' and attempted_pass == 'admin':
+            return redirect(url_for('admin_home'))
+        else:
+            error = 'Invalid username or password!'
+    return render_template('login.html', error=error)
+
+@app.route('/admin_home')
+def admin_home():
+    return 'Hello, welcome to our app'
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
