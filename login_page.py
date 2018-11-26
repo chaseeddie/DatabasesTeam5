7ec7df5e17817a703c3d25c3044bd0e8e21036ef
chaseeddie <38984786+chaseeddie@users.noreply.github.com>
2018-11-26 15:27:59 -0600

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request
+import pymysql
+
 app = Flask(__name__)
+
+#  DATABASE CONFIG #
+
+conn = pymysql.connect(host='127.0.0.1', user='root', password='root', db='version1')
+
+cursor = conn.cursor()
 
 # this is our homepage
 
@@ -9,21 +17,22 @@ def login():
     if request.method == 'POST':
         attempted_user = request.form['username']
         attempted_pass = request.form['password']
+        result_value = cursor.execute(
+            "SELECT * FROM admins WHERE username = %s AND password = %s GROUP BY username",
+            (attempted_user, attempted_pass)
+        )
+         #gets the number of rows affected by the command executed
+        if result_value > 0:
+        #if attempted_user == 'admin' and attempted_pass == 'admin':
 
-        if attempted_user == 'admin' and attempted_pass == 'admin':
             return redirect(url_for('admin_home'))
         else:
             error = 'Invalid username or password!'
-    return render_template('login.html', error=error)
+    return render_template('login.html', error= error)
 
 @app.route('/admin_home')
 def admin_home():
-    return 'Hello, welcome to our app'
-
-
-
-
-
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
