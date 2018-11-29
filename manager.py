06f6a398b38dbc82e7144d4afd4fd01448735f20
kittdee <sincerelydns@gmail.com>
2018-11-29 17:14:14 -0600

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+db = pymysql.connect(host='localhost', user='root', password='password', db='version1')
+
+
 @app.route("/")
 def manage():
     return render_template("manage.html")
+
 @app.route("/vendors")
 def vendors():
     return render_template("vendors.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
 
