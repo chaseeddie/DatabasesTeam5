9b3ab6fa57b97bd92b1f69662c1a3fc5bbacb340
kittdee <sincerelydns@gmail.com>
2018-12-02 14:08:42 -0600

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
+from pymysql import cursors
+import pymysql
 
 app = Flask(__name__)
 
 db = pymysql.connect(host='localhost', user='root', password='password', db='version1')
 
 cursor = db.cursor()
-
-cursor.execute('CREATE TABLE IF NOT EXISTS Materials (itemCode TEXT, itemName TEXT, description TEXT, price TEXT, manufacDate TEXT, expireDate TEXT, manufactorer TEXT, image TEXT, supplier TEXT, altItem TEXT)')
-print('table created')
 
 @app.route("/")
 def manage():
@@ -17,6 +16,9 @@ def manage():
 def vendors():
     return render_template("vendors.html")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+@app.route("/request")
+def request():
+    return render_template("request.html")
 
+if __name__ == "__main__":
+    app.run(debug=True)
