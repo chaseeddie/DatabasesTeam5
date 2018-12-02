b1bdf4b752059ba0632d6e4c2adf3b611a8a5bc8
kittdee <sincerelydns@gmail.com>
2018-12-02 15:55:19 -0600

    Update manager.py

--- manager.py
+++ manager.py
@@ -12,7 +12,7 @@ cursor = conn.cursor()
 cursor.execute
 
 @app.route('/', methods = ['POST','GET'])
-def manageItems():
+def manage():
     if request.method == 'POST':
         itemID = request.form['itemID']
         itemName = request.form['itemName']
