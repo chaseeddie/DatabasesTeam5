ff61596dd695eff004a26c02a4525c23f2fea4f9
chaseeddie <38984786+chaseeddie@users.noreply.github.com>
2018-12-02 19:23:25 -0600

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -10,7 +10,7 @@ conn = pymysql.connect(host='localhost', user='root', password='root', db='versi
 cursor = conn.cursor()
 
 
-# this is our homepage
+# LOGIN PAGE
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -18,23 +18,132 @@ def login():
     if request.method == 'POST':
         attempted_user = request.form['username']
         attempted_pass = request.form['password']
+
+        # QUERIES TO CHECK ROLES
+
         result_value = cursor.execute(
-            "SELECT * FROM admins WHERE BINARY username = %s AND BINARY password = %s GROUP BY username",
+            "SELECT * FROM user_info WHERE BINARY username = %s AND BINARY password = %s GROUP BY username",
             (attempted_user, attempted_pass)
+        )
+        result_admin = cursor.execute(
+            """SELECT * FROM user_info WHERE BINARY username = %s AND 
+            BINARY password = %s AND role = %s GROUP BY username""",
+            (attempted_user, attempted_pass, 'admin')
+        )
+        result_manager = cursor.execute(
+            """SELECT * FROM user_info WHERE BINARY username = %s AND 
+            BINARY password = %s AND role = %s GROUP BY username""",
+            (attempted_user, attempted_pass, 'manager')
+        )
+        result_vendor = cursor.execute(
+            """SELECT * FROM user_info WHERE BINARY username = %s AND 
+            BINARY password = %s AND role = %s GROUP BY username""",
+            (attempted_user, attempted_pass, 'vendor')
         )
         # gets the number of rows affected by the command executed
         if result_value > 0:
-            # if attempted_user == 'admin' and attempted_pass == 'admin':
-
-            return redirect(url_for('admin_home'))
+            if result_admin > 0:
+                return redirect(url_for('admin_home'))
+            elif result_manager > 0:
+                return redirect(url_for('manager_home'))
+            elif result_vendor > 0:
+                return redirect(url_for('vendor_home'))
+            else:
+                return redirect(url_for('floor_home'))
         else:
             error = 'Invalid username or password!'
     return render_template('login.html', error=error)
 
 
+@app.route('/vendor_home', methods=['GET', 'POST'])
+def vendor_home():
+    return render_template('home_vendor.html')
+
+@app.route('/manage_materials', methods=['GET', 'POST'])
+def manage_materials():
+    if request.method == 'POST':
+        itemCode = request.form['itemCode']
+        itemName = request.form['itemName']
+        itemQt = request.form['itemQt']
+        itemDescription = request.form['itemDescription']
+        itemPrice = request.form['itemPrice']
+        itemSupplier = request.form['itemSupplier']
+        itemDateMan = request.form['itemDateMan']
+        itemExpDate = request.form['itemExpDate']
+        itemImage = request.form['itemImage']
+        cursor.execute(
+            """INSERT INTO items(code, name, quantity, description, price, supplier, 
+            dateManufactured, expirationDate, image)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+            (itemCode, itemName, itemQt, itemDescription, itemPrice, itemSupplier, itemDateMan,
+             itemExpDate, itemImage)
+        )
+        conn.commit()
+        return render_template('manage_materials.html')
+    return render_template('manage_materials.html')
+
+@app.route('/manager_home', methods=['GET', 'POST'])
+def manager_home():
+    return render_template('home_manager.html')
+
+@app.route('/manage_vendor', methods=['GET', 'POST'])
+def manage_vendor():
+    if request.method == 'POST':
+
+        venName = request.form['venName']
+        minorquantity = request.form['minOrdQuantity']
+        datepurchased = request.form['datePurchased']
+        itempurchased = request.form['itemPurchased']
+        quality = request.form['quality']
+        emailaddress = request.form['emailaddress']
+        phonenum = request.form['phonenum']
+
+        cursor.execute(
+            """INSERT INTO vendors(VenName, MinOrdQuantity, DatePurchased, ItemsPurchased, 
+            Quality, EmailAddress, PhoneNum)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)""",
+            (venName, minorquantity, datepurchased, itempurchased,
+             quality, emailaddress, phonenum)
+        )
+        conn.commit()
+
+        return render_template('manage_vendor.html')
+    return render_template('manage_vendor.html')
+
+
+
+@app.route('/floor_home')
+def floor_home():
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM user_info')
+    floor_data = cursor.fetchall()
+
+    return render_template('home_shopfloor.html', data=floor_data)
+
+
+
 @app.route('/admin_home')
 def admin_home():
-    return render_template('home.html')
+    return render_template('home_admin.html')
+
+@app.route('/shipping_details', methods = ['GET','POST'])
+def shipping_details():
+    if request.method == 'POST':
+        shipAddress = request.form['shipAddress']
+        contactEmail = request.form['contactEmail']
+        contactPhone = request.form['contactPhone']
+        paymentMethod = request.form['paymentMethod']
+
+        cursor.execute(
+            """INSERT INTO shippingdetails(shipAddress, contactEmail, contactPhone, paymentMethod)
+            VALUES (%s, %s, %s, %s)""",
+            (shipAddress, contactEmail, contactPhone, paymentMethod)
+        )
+        conn.commit()
+
+        return redirect(url_for('shipping_details'))
+
+    return render_template('admin_shipping_details.html')
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -55,8 +164,8 @@ def registration():
         )
         conn.commit()
 
-        return render_template('registration_test.html')
-    return render_template('registration_test.html')
+        return redirect(url_for('registration'))
+    return render_template('registration.html')
 
 
 if __name__ == "__main__":
